de474f4c4879acfdc98af632e305a84723991ee3
NycolasSM <nycolassantana00@gmail.com>
2022-02-28 13:06:24 -0300

    add frontEnd model, and backend

---
+++ cypress/integration/ongs.spec.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe('Ongs', () => {
+  it('devem poder realizar um cadastro', () => {
+
+  });
+
+  it('deve poder realizar um login no sistema', () => {
+    
+  });
+})
