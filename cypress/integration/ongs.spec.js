a121b5a2544da1d51dd3be4b1f873fe761fb9567
NycolasSM <nycolassantana00@gmail.com>
2022-02-28 14:16:21 -0300

    first tests

--- cypress/integration/ongs.spec.js
+++ cypress/integration/ongs.spec.js
@@ -2,7 +2,16 @@
 
 describe('Ongs', () => {
   it('devem poder realizar um cadastro', () => {
+    cy.visit('http://localhost:3000/register');
+    // cy.get - busca um elemento
+    // .type - insere um texto
+    cy.get('[data-cy=name]').type('Inserindo Texto Teste')
+    cy.get('[data-cy=email]').type('Email@teste.com')
+    cy.get('[data-cy=whatsapp]').type('139999-9999')
+    cy.get('[data-cy=city]').type('Jacupiranga')
+    cy.get('[data-cy="uf"]').type('SP')
 
+    cy.get('[data-cy="submit"]').click()
   });
 
   it('deve poder realizar um login no sistema', () => {
