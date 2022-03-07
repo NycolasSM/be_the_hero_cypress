142e23cfe7683a7d6f9c5429f44bb873b86f26dc
NycolasSM <nycolassantana00@gmail.com>
2022-03-07 13:19:19 -0300

    add Allure and configure then

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -20,3 +20,11 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
+
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+// import allureWriter from "@shelex/cypress-allure-plugin/writer";
+
+module.exports = (on, config) => {
+    allureWriter(on, config);
+    return config;
+};
